9782d8db227a5f901fb7ca329a6007b36e46bf29
xXxMrDEDOxXx <100015448+xXxMrDEDOxXx@users.noreply.github.com>
2025-05-25 12:56:45 +0700

    Change commit

--- tank 1990/main.cpp
+++ tank 1990/main.cpp
@@ -1,8 +1,9 @@
 #include "Game.h"
 #include <windows.h>
 int main() {
-    // สร้างเกมและเริ่มเล่น
+    // ทำให้ terminal ใน visual studio code อ่านภาษาไทยออก
     system("chcp 65001 > nul");
+    // สร้างเกมและเริ่มเล่น
     Game game;
     game.run();
     return 0;
